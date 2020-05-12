2f04bb3adf443b20c57e770038714a4a5a4e106b
Julian Tölle <julian@narando.de>
2020-05-12 18:32:37 +0200

    feat: export AxiosResponseCustomConfig for accurate typings

--- lib/index.ts
+++ lib/index.ts
@@ -2,3 +2,4 @@ export { AxiosInterceptor } from "./axios-interceptor";
 
 export type { AxiosFulfilledInterceptor } from "./interfaces/axios-fulfilled-interceptor";
 export type { AxiosRejectedInterceptor } from "./interfaces/axios-rejected-interceptor";
+export type { AxiosResponseCustomConfig } from "./interfaces/axios-response-custom-config";
