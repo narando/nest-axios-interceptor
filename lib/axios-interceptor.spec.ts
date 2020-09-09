3bfc057dc0e92d4a5edff753ae68f09ec54bf810
Julian Tölle <julian@narando.de>
2020-09-09 12:58:09 +0200

    test: replace deprecated eslint rule

--- lib/axios-interceptor.spec.ts
+++ lib/axios-interceptor.spec.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/ban-ts-ignore */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 
 import { HttpService, Injectable } from "@nestjs/common";
 import { Test } from "@nestjs/testing";
